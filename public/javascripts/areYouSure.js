6598233ffaa8cf1026497a664bd3920803eb738c
olsen601 <tedssmith2556@gmail.com>
2017-11-22 13:49:17 -0600

    added id for delete button

--- public/javascripts/areYouSure.js
+++ public/javascripts/areYouSure.js
@@ -1,4 +1,4 @@
-deleteButton.addEventListener("click", function(event){
+deleteBtn.addEventListener("click", function(event){
         var sure = confirm("Are you sure you want to delete this bird?");
         if (!sure) {
             event.preventDefault();   // don't actually click the button or send a request to the server.
